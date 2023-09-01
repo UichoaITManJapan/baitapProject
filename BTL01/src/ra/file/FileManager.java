8ba214d2361a4fd42de8d1a310e97ca0c77708ff
UichoaITManJapan <132963966+UichoaITManJapan@users.noreply.github.com>
2023-09-01 14:05:15 +0900

    Add files via upload

---
+++ BTL01/src/ra/file/FileManager.java
@@ -0,0 +1,90 @@
+package ra.file;
+
+import ra.entity.Book;
+import ra.entity.Category;
+
+import java.io.*;
+import java.util.List;
+
+public class FileManager {
+    private static final String CATEGORY_FILE = "categories.txt";
+    private static final String BOOK_FILE = "books.txt";
+    public static <T> void saveDataToFile(String fileName, List<T> dataList){
+        File file = new File(fileName);
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream(file);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(dataList);
+            oos.flush();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (oos != null){
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    public static <T> List<T> loadDataFromFile(String fileName){
+        File file = new File(fileName);
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        List<T> dataList = null;
+        try {
+            fis = new FileInputStream(file);
+            ois = new ObjectInputStream(fis);
+            if (ois.readObject() != null){
+                return (List<T>) ois.readObject();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (ois != null){
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return dataList;
+    }
+    public static void writeDataToCategoryFile(List<Category> categoryList){
+        saveDataToFile(CATEGORY_FILE,categoryList);
+    }
+    public static List<Category> readDataFromCategoryFile(){
+        return loadDataFromFile(CATEGORY_FILE);
+    }
+
+    public static void writeDataToBookFile(List<Book> bookList){
+        saveDataToFile(BOOK_FILE,bookList);
+    }
+    public static List<Book> readDataFromBookFile(){
+        return loadDataFromFile(BOOK_FILE);
+    }
+}
